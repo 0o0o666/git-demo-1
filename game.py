8397ad664f07ca8ab2bfbecc9fa44d7fc917c0dc
scc <scc@gmail.com>
2024-07-13 16:09:15 +0800

    7/13

--- game.py
+++ game.py
@@ -5,6 +5,12 @@ print(x)
 for __ in range(15):
     y = int(input("請猜數字"))
     if x == y:
-        print("猜對了")
+        print(f"猜對了,猜了{i+1}次")
+        break
     else:
-        print("才錯了")
+        if x > y:
+            print("猜高點")
+        else:
+            print("猜低點")
+if x != y:
+    print(f"答案為{x},猜了{i+1}次")
